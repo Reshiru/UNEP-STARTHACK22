212e4acc3599d7d2b036ceca5ee2ba7b4b0e0fb9
Joel Kessler <joelkessler1412@gmail.com>
2022-03-30 01:17:45 +0200

    batch size

--- TerrAI/DeepModel.py
+++ TerrAI/DeepModel.py
@@ -56,7 +56,7 @@ class DeepModel():
 
     def append_dense_layer(self, x, prefix):
         self.layer_index += 1
-        x = Dense(256, activation='relu', name=f"{prefix}-DENSE-{self.layer_index}")(x)
+        x = Dense(32, activation='relu', name=f"{prefix}-DENSE-{self.layer_index}")(x)
         x = BatchNormalization(name=f"{prefix}-NORM-{self.layer_index}")(x)
         x = Dropout(0.5, name=f"{prefix}-DROP-{self.layer_index}")(x)
         return x
