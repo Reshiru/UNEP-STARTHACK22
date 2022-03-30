2fd09ac9d26d48031359f0af232f90e4aa8b8fa4
Joel Kessler <joelkessler1412@gmail.com>
2022-03-30 21:28:23 +0200

    search for best model

--- TerrAI/DeepModel.py
+++ TerrAI/DeepModel.py
@@ -43,32 +43,32 @@ class DeepModel(HyperModel):
     
     def run(self):
         model = models.Sequential()
-        model.add(Conv2D(input_shape=(9, 5, 5), filters=32, kernel_size=(3,3), padding="same"))
+        model.add(Conv2D(input_shape=(9, 5, 5), filters=64, kernel_size=(3,3), padding="same"))
         model.add(Activation('relu'))
-        model.add(Dropout(0.2))
-        model.add(Conv2D(filters=8, kernel_size=(3,3), strides=(1,1), padding="same"))
+        model.add(Dropout(0.1))
+        model.add(Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same"))
         model.add(Activation('relu'))
-        model.add(Dropout(0.2))
+        model.add(Dropout(0.1))
         model.add(Flatten())
         model.add(Dense(16))
         model.add(Activation('relu'))
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.1))
         model.add(Dense(self.output_size, activation="sigmoid"))
         model.compile(optimizer=optimizers.Adam(), loss=tf.keras.losses.BinaryCrossentropy(), metrics=[binary_accuracy, Recall(), Precision(), f1_score])
         return model
     
     def build(self, hp):
         model = models.Sequential()
-        model.add(Conv2D(input_shape=(9, 5, 5), filters=hp.Int('f_1',min_value=4,max_value=64,step=4), kernel_size=(3,3), padding="same"))
+        model.add(Conv2D(input_shape=(9, 5, 5), filters=hp.Int('f_1',min_value=4,max_value=64,step=4,default=32), kernel_size=(3,3), padding="same"))
         model.add(Activation('relu'))
-        model.add(Dropout(hp.Float('d_1',min_value=0.1,max_value=0.5,step=0.1)))
-        model.add(Conv2D(filters=hp.Int('f_2',min_value=4,max_value=32,step=2), kernel_size=(3,3), strides=(1,1), padding="same"))
+        model.add(Dropout(hp.Float('d_1',min_value=0.1,max_value=0.5,step=0.1,default=0.2)))
+        model.add(Conv2D(filters=hp.Int('f_2',min_value=4,max_value=64,step=2,default=8), kernel_size=(3,3), strides=(1,1), padding="same"))
         model.add(Activation('relu'))
-        model.add(Dropout(hp.Float('d_1',min_value=0.1,max_value=0.5,step=0.1)))
+        model.add(Dropout(hp.Float('d_2',min_value=0.1,max_value=0.5,step=0.1,default=0.2)))
         model.add(Flatten())
-        model.add(Dense(16))
+        model.add(Dense(hp.Int('f_3',min_value=4,max_value=64,step=4,default=16)))
         model.add(Activation('relu'))
-        model.add(Dropout(hp.Float('d_1',min_value=0.1,max_value=0.5,step=0.1)))
+        model.add(Dropout(hp.Float('d_3',min_value=0.1,max_value=0.5,step=0.1,default=0.5)))
         model.add(Dense(self.output_size, activation="sigmoid"))
         model.compile(optimizer=optimizers.Adam(), loss=tf.keras.losses.BinaryCrossentropy(), metrics=[binary_accuracy, f1_score])
         return model
@@ -77,6 +77,7 @@ class DeepModel(HyperModel):
         search_opt = BayesianOptimization(hypermodel=self, objective=Objective("val_f1_score", direction="max"), max_trials=256, executions_per_trial=1, overwrite=False)
         search_opt.search(train_dataset, validation_data=test_dataset, epochs=20, batch_size=batch_size, class_weight=class_weight, verbose=0)
         best_model = search_opt.get_best_models(num_models=1)[0]
+        print(search_opt.get_best_hyperparameters(num_trials=1)[0].values)
         best_model.summary()
         return search_opt
         
